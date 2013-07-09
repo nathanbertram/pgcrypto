ce4fe516502ce1e1beded08c76ae39bc22d77c7f
Nathan Bertram <nbertram@gmail.com>
2013-07-09 13:15:03 -0500

    Not sure about this guy yet

--- lib/pgcrypto/column.rb
+++ lib/pgcrypto/column.rb
@@ -1,15 +1,14 @@
 module PGCrypto
   class Column < ActiveRecord::Base
     self.table_name = 'pgcrypto_columns'
-    after_save :set_owner_table
+    # before_save :set_owner_table
     belongs_to :owner, :autosave => false, :inverse_of => :pgcrypto_columns, :polymorphic => true
 
     default_scope { select(%w(id owner_id owner_type owner_table name)) }
 
     protected
-
     def set_owner_table
-      update_attribute(:owner_table, self.owner.class.table_name)
+      # self.owner_table = self.owner.class.table_name
     end
   end
 end
