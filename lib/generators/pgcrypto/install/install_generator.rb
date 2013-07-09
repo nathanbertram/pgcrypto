4bc5f09d908a275ea76d74d80ac5ebfd8a87f55d
Nathan Bertram <nbertram@gmail.com>
2013-07-09 11:37:13 -0500

    Okay, generator is now working in rails 4!

--- lib/generators/pgcrypto/install/install_generator.rb
+++ lib/generators/pgcrypto/install/install_generator.rb
@@ -1,11 +1,9 @@
 require 'rails/generators/migration'
-# require 'rails/generators/active_record/migration'
 
 module Pgcrypto
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      # extend ActiveRecord::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
 
@@ -16,6 +14,11 @@ module Pgcrypto
       def create_initializer
         copy_file("initializer.rb", "config/initializers/pgcrypto.rb")
       end
+
+      def self.next_migration_number(path)
+        @migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i.to_s
+      end
+
     end
   end
 end
