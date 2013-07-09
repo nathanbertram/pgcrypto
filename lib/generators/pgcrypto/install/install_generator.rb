bd6c98e240e4b4781cb5464abddfb530a0eb0c58
Nathan Bertram <nbertram@gmail.com>
2013-07-09 11:34:02 -0500

    Not sure if this is needed

--- lib/generators/pgcrypto/install/install_generator.rb
+++ lib/generators/pgcrypto/install/install_generator.rb
@@ -1,11 +1,11 @@
 require 'rails/generators/migration'
-require 'rails/generators/active_record/migration'
+# require 'rails/generators/active_record/migration'
 
 module Pgcrypto
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      extend ActiveRecord::Generators::Migration
+      # extend ActiveRecord::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
 
